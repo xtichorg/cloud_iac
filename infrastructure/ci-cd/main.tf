ece94abb98eb04030525f5fbdc5a09843c7a1f0f
Dima Khristich <dkhristich@gmail.com>
2025-11-03 20:55:41 +0200

    state

--- infrastructure/ci-cd/main.tf
+++ infrastructure/ci-cd/main.tf
@@ -1,11 +1,5 @@
 terraform {
   required_version = "~> 1.13" # Ensure that the Terraform version is 1.0.0 or higher
-  backend "s3" {
-    bucket       = "tf-state-bucket-iac"
-    key          = "xtichorg/ci-cd"
-    region       = "us-east-1"
-    use_lockfile = true
-  }
   required_providers {
     aws = {
       source  = "hashicorp/aws" # Specify the source of the AWS provider
